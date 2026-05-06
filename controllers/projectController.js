ed6f998becf562656e65ede249f87cf75d9c8368
proximon1 <heredidanielofficial@gmail.com>
2026-05-06 21:15:40 +0200

    previous/next project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,22 +1,46 @@
-import { getProjectBySlug, getProjectItems, getProjectTags, getMainImage } from "../models/projectModel.js";
+import {
+  getProjectBySlug,
+  getProjectItems,
+  getProjectTags,
+  getMainImage,
+  getAllProjects
+} from "../models/projectModel.js";
 
 export const renderProject = async (req, res) => {
   const { slug } = req.params;
 
   const project = await getProjectBySlug(slug);
-  const tags = await getProjectTags(project.id);
-  const mainImage = await getMainImage(project.id);
 
   if (!project) {
     return res.status(404).send("Not found");
   }
 
+  const tags = await getProjectTags(project.id);
+  const mainImage = await getMainImage(project.id);
   const items = await getProjectItems(project.id);
+
+  const projects = await getAllProjects();
+
+  const currentIndex = projects.findIndex(
+    p => p.slug === project.slug
+  );
+
+  const previousProject =
+    currentIndex > 0
+      ? projects[currentIndex - 1]
+      : null;
+
+  const nextProject =
+    currentIndex < projects.length - 1
+      ? projects[currentIndex + 1]
+      : null;
 
   res.render("projects/project", {
     project,
     items,
     tags,
-    mainImage
+    mainImage,
+    previousProject,
+    nextProject
   });
 };
