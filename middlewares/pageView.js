485dc2fcd7a703dee78b1cc2001c7666a52dc2c2
proximon1 <heredidanielofficial@gmail.com>
2026-06-25 11:44:04 +0200

    page visit whitelist

--- middlewares/pageView.js
+++ middlewares/pageView.js
@@ -2,11 +2,24 @@ import { savePageView } from "../models/adminModel.js";
 
 export const pageView = async (req, res, next) => {
   try {
+    // Ezeket soha ne számoljuk
     if (
       req.path.startsWith("/admin") ||
       req.path.startsWith("/api") ||
       req.path.includes(".")
     ) {
+      return next();
+    }
+
+    const allowedPaths = [
+      "/",
+      "/contact",
+      "/privacy"
+    ];
+
+    const isProjectPage = req.path.startsWith("/projects/");
+
+    if (!allowedPaths.includes(req.path) && !isProjectPage) {
       return next();
     }
 
